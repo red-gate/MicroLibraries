442d80499e3cdec4134ff48baaac44b5f26a4570
jeff.foster <jeff.foster@red-gate.com>
2018-09-05 12:53:44 +0100

    Add new library using script.

---
+++ Source/ULibs.Win32/Class1.cs
@@ -0,0 +1,9 @@
+﻿/***using System.Diagnostics.CodeAnalysis;***/
+
+namespace /***$rootnamespace$.***/ULibs.Win32
+{
+    /***[ExcludeFromCodeCoverage]***/
+    internal class Class1
+    {
+    }
+}
---
+++ Source/ULibs.Win32/RELEASENOTES.md
@@ -0,0 +1,7 @@
+﻿# ULibs.Win32 release notes
+
+## 0.1.0
+
+### Features
+
+- Describe the first feature here.
