ec9958a9c3f9ac0f7743ac71b9156038b073b8bd
yarhamjohn <yarhamjohn@gmail.com>
2020-03-16 10:46:51 +0000

    Move attribute class to separate file

--- Source/ULibs.TinyJsonDeser/JsonDeserializer.cs
+++ Source/ULibs.TinyJsonDeser/JsonDeserializer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-/***using System.Diagnostics.CodeAnalysis;***/
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -657,22 +656,5 @@ namespace /***$rootnamespace$.***/ULibs.TinyJsonDeser
             output = default;
             return false;
         }
-    }
-}
-
-namespace System.Diagnostics.CodeAnalysis
-{
-    /// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter will not be null even if the corresponding type allows it.</summary>
-    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    internal sealed class NotNullWhenAttribute : Attribute
-    {
-        /// <summary>Initializes the attribute with the specified return value condition.</summary>
-        /// <param name="returnValue">
-        /// The return value condition. If the method returns this value, the associated parameter will not be null.
-        /// </param>
-        internal NotNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
-
-        /// <summary>Gets the return value condition.</summary>
-        internal bool ReturnValue { get; }
     }
 }
